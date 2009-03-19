984cd310da35135a4b0d2a3159c42b003f011ab9
danski <dan@angryamoeba.co.uk>
2009-03-19 16:03:53 +0000

    deploy script tweak

--- scripts/jekyll.sh
+++ scripts/jekyll.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Running jekyll recompile in directory" `pwd` '<br />';
 echo "Updating from git origin...<br />";
-git pull
+
+cd `pwd` && git pull;
+
 echo "Recompiling and highlighting with Jekyll...<br />";
 
 jekyll --pygments "`pwd`/_jekyll_singlecell" "`pwd`/singlecell"
+
 exit 0;
